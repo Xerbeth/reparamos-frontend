95f91c655e7984ac35779d4174e67f4785fcd797
Herman Andres <heanfig@gmail.com>
2021-07-18 01:56:52 -0500

    Se crea tabla para listar clientes

--- src/modules/tables/containers/gestion-clientes/gestion-clientes.component.ts
+++ src/modules/tables/containers/gestion-clientes/gestion-clientes.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, TemplateRef } from '@angular/core';
 import { Client } from '@modules/tables/models/clients-response.model';
 import { ClientesService } from '@modules/tables/services/clientes.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
 import Swal from 'sweetalert2';
 
@@ -16,7 +18,8 @@ export class GestionClienteComponent implements OnInit {
 
     constructor(
         protected clientsService: ClientesService,
-        private changeDetectorRef: ChangeDetectorRef
+        private changeDetectorRef: ChangeDetectorRef,
+        private modalService: NgbModal
     ) {}
     
     ngOnInit() {
@@ -27,5 +30,18 @@ export class GestionClienteComponent implements OnInit {
                 this.changeDetectorRef.detectChanges();
             }
         })
+    }
+
+    public open(content: TemplateRef<any>) {
+        this.modalService.open(content, { 
+            ariaLabelledBy: 'modal-basic-title', 
+            windowClass: 'reparamos-modal' 
+        }).result.then(
+            (result) => {
+                console.warn(`Closed with: ${result}`);
+            }, (reason) => {
+                console.warn(`dissmised: ${reason}`)
+            }
+        );
     }
 }
